d899fc41de1cae49d5b5143e84798a5394d36d89
*Collins <nyamburycollins@mail.com>
2025-04-27 17:39:26 +0300

    Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <div></div>
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={false}
+        aria-checked={false}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+    </div>
   )
 }
 
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,7 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+test("pizza checkbox is initially unchecked", () => {})
+
+test("toppings list initially contains only cheese", () => {})
+
